c08aba6be2fe75c0d5baf74c68031b8aa1139b55
rhewap10 <risipp3@gmail.com>
2025-10-05 22:02:44 +1100

    Add files via upload

---
+++ event_db.js
@@ -0,0 +1,34 @@
+﻿const sql = require('mssql');
+
+const config = {
+    server: 'NICE', 
+    database: 'charityevents_db',
+    authentication: {
+        type: 'ntlm', 
+        options: {
+            domain: process.env.USERDOMAIN, 
+            userName: process.env.USERNAME, 
+            password: '' 
+        }
+    },
+    options: {
+        encrypt: false,
+        trustServerCertificate: true
+    },
+    port: 1433
+};
+
+
+
+const poolPromise = new sql.ConnectionPool(config)
+    .connect()
+    .then(pool => {
+        console.log('✅ MSSQL Connected using Windows Authentication');
+        return pool;
+    })
+    .catch(err => {
+        console.error('❌ MSSQL Connection Failed:', err);
+        throw err;
+    });
+
+module.exports = { sql, poolPromise };
